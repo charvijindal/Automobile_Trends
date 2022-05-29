033551785c387eafc9aac005b5462d55a11bec81
charvijindal <charvijindal@gmail.com>
2022-05-29 21:19:21 +0530

    final changes

--- multipage.py
+++ multipage.py
@@ -1,27 +1,13 @@
-"""
-This file is the framework for generating multiple Streamlit applications 
-through an object oriented framework. 
-"""
-
-# Import necessary libraries 
 import streamlit as st
 
-# Define the multipage class to manage the multiple apps in our program 
+# To manage multiple pages 
 class MultiPage: 
-    """Framework for combining multiple streamlit applications."""
 
     def __init__(self) -> None:
-        """Constructor class to generate a list which will store all our applications as an instance variable."""
         self.pages = []
     
     def add_page(self, title, func) -> None: 
-        """Class Method to Add pages to the project
-
-        Args:
-            title ([str]): The title of page which we are adding to the list of apps 
-            
-            func: Python function to render this page in Streamlit
-        """
+        #add pages to the app
 
         self.pages.append(
             {
